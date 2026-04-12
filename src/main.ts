dbab10f8b440f120dfd33993683bb8fcdf67add0
Jason <development@pnxl.dev>
2026-04-12 21:06:57 +0700

    add internationalisation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
+import { createI18n } from "vue-i18n";
 
 import "./assets/index.css";
 
-createApp(App).use(router).mount("#app");
+const i18n = createI18n({
+  locale: "id",
+  fallbackLocale: "en",
+  messages: {},
+});
+
+createApp(App).use(router).use(i18n).mount("#app");
