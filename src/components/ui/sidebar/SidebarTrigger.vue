c6464dfbfd9d0ebfe1064b583e3041290a190e54
Jason <development@pnxl.dev>
2026-04-12 16:34:16 +0700

    resize sidebar trigger

--- src/components/ui/sidebar/SidebarTrigger.vue
+++ src/components/ui/sidebar/SidebarTrigger.vue
@@ -21,7 +21,7 @@ const { toggleSidebar } = useSidebar();
     :class="cn('h-7 w-7', props.class)"
     @click="toggleSidebar"
   >
-    <IconLayoutSidebar />
+    <IconLayoutSidebar class="size-5" />
     <span class="sr-only">Toggle Sidebar</span>
   </Button>
 </template>
